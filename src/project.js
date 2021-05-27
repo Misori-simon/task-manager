edd09a464500e81c8b871dd5a5fbfd4b80a92b86
Misori-simon <misorisimon@gmail.com>
2021-05-27 05:56:34 +0100

    Add Project object using function prototypes - Add Project object methods - Add test for Project object methods

--- src/project.js
+++ src/project.js
@@ -3,4 +3,18 @@ function Project(title) {
   this.todos = [];
 }
 
+Project.prototype.addTodo = function addTodo(todoObj) {
+  this.todos.push(todoObj);
+  return this.todos.length;
+};
+
+Project.prototype.removeTodo = function removeTodo(index) {
+  this.todos.splice(index, 1);
+  return this.todos.length;
+};
+
+Project.prototype.count = function count() {
+  return this.todos.length;
+};
+
 export default Project;
--- test/project.test.js
+++ test/project.test.js
@@ -8,4 +8,19 @@ describe('Project Object', () => {
   it('should have an empty array of empty Todo items', () => {
     expect(testData.project.todos.length).toBe(0);
   });
+
+  it('should add a Todo item to Project todos array ', () => {
+    testData.project.addTodo(testData.todo);
+    testData.project.addTodo(testData.todo);
+    expect(testData.project.todos.length).toBe(2);
+  });
+
+  it('should removed a Todo item to Project todos array ', () => {
+    testData.project.removeTodo(0);
+    expect(testData.project.todos.length).toBe(1);
+  });
+
+  it('should return a count of Todo items in a Project ', () => {
+    expect(testData.project.count()).toBe(1);
+  });
 });
