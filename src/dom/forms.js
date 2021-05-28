af4d8c418b6ad8008f8f58334297d17b2557b956
Misori-simon <misorisimon@gmail.com>
2021-05-28 09:55:27 +0100

    Add todoForm function  - Add function to forms modules to make forms for creating new todo items

--- src/dom/forms.js
+++ src/dom/forms.js
@@ -18,4 +18,97 @@ function projectForm() {
   return form;
 }
 
-export default projectForm;
+function todoForm() {
+  const form = document.createElement('form');
+  form.setAttribute('class', 'todo-form');
+
+  const titleField = document.createElement('input');
+  titleField.setAttribute('type', 'text');
+  titleField.setAttribute('class', 'text-field');
+  titleField.setAttribute('placeholder', 'Todo Title');
+
+  const descField = document.createElement('input');
+  descField.setAttribute('type', 'text');
+  descField.setAttribute('class', 'text-field');
+  descField.setAttribute('placeholder', 'Todo Description');
+
+  const dateField = document.createElement('input');
+  dateField.setAttribute('type', 'date');
+  dateField.setAttribute('class', 'text-field');
+  dateField.setAttribute('placeholder', 'Todo Description');
+
+  const priority = document.createElement('div');
+  priority.setAttribute('class', 'priority-wrap');
+
+  const highPriority = document.createElement('span');
+  highPriority.setAttribute('class', 'priority-item');
+
+  const highLabel = document.createElement('p');
+  highLabel.setAttribute('class', 'priority-label');
+  highLabel.innerHTML = 'high';
+
+  const highField = document.createElement('input');
+  highField.setAttribute('type', 'radio');
+  highField.setAttribute('name', 'priority');
+
+  highPriority.appendChild(highLabel);
+  highPriority.appendChild(highField);
+
+  const mediumPriority = document.createElement('span');
+  mediumPriority.setAttribute('class', 'priority-item');
+
+  const mediumLabel = document.createElement('p');
+  mediumLabel.setAttribute('class', 'priority-label');
+  mediumLabel.innerHTML = 'medium';
+
+  const mediumField = document.createElement('input');
+  mediumField.setAttribute('type', 'radio');
+  mediumField.setAttribute('name', 'priority');
+
+  mediumPriority.appendChild(mediumLabel);
+  mediumPriority.appendChild(mediumField);
+
+  const lowPriority = document.createElement('span');
+  lowPriority.setAttribute('class', 'priority-item');
+
+  const lowLabel = document.createElement('p');
+  lowLabel.setAttribute('class', 'priority-label');
+  lowLabel.innerHTML = 'low';
+
+  const lowField = document.createElement('input');
+  lowField.setAttribute('type', 'radio');
+  lowField.setAttribute('name', 'priority');
+
+  lowPriority.appendChild(lowLabel);
+  lowPriority.appendChild(lowField);
+
+  priority.appendChild(highPriority);
+  priority.appendChild(mediumPriority);
+  priority.appendChild(lowPriority);
+
+  const notesField = document.createElement('textarea');
+  notesField.setAttribute('type', 'text');
+  notesField.setAttribute('class', 'text-area');
+  notesField.setAttribute('placeholder', 'Add Notes');
+
+  const submitBtn = document.createElement('button');
+  submitBtn.setAttribute('type', 'button');
+  submitBtn.setAttribute('class', 'add-btn');
+  submitBtn.innerHTML = 'ADD';
+
+  form.appendChild(titleField);
+  form.appendChild(descField);
+  form.appendChild(dateField);
+  form.appendChild(priority);
+  form.appendChild(notesField);
+  form.appendChild(submitBtn);
+
+  return form;
+}
+
+const forms = {
+  project: projectForm(),
+  todo: todoForm(),
+};
+
+export default forms;
